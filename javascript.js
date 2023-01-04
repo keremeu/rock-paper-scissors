6df6c9066a7a53212ca87b05f85987526628485b
keremeu <keremeu@gmail.com>
2023-01-05 02:36:43 +0300

    Final form

--- javascript.js
+++ javascript.js
@@ -1,8 +1,59 @@
-const choices = ["rock", "paper", "scissors"];
-let computerChoice = ""
+let winCount = 0;
+let loseCount = 0;
 
-function getComputerChoice() 
-{
-    computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerChoice);
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound() {
+    const computerChoice = getComputerChoice();
+    const playerChoice = prompt("What is your choice?", "").toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        console.log("It is a tie");
+    }
+    else if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose, paper beats rock.");
+        loseCount++;
+    }
+    else if (playerChoice === "paper" && computerChoice === "rock"){
+        console.log("You win, paper beats rock.");
+        winCount++;
+    }
+    else if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win, rock beats scissors.");
+        winCount++;
+    }
+    else if (playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose, rock beats scissors.");
+        loseCount++;
+    }
+    else if (playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win, scissors beats paper.");
+        winCount++;
+    }
+    else if (playerChoice === "paper" && computerChoice === "scissors.") {
+        console.log("You lose, scissors beat paper.");
+        loseCount++;
+    }
+    else {
+        console.log("Not a valid choice.")
+    } 
+}
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (winCount > loseCount) {
+        alert("You won!");
+    }
+    else if (winCount < loseCount) {
+        alert("You lost :(");
+    }
+    else {
+        alert("It is a tie.")
+    }
 }
